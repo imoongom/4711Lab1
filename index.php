d6d745e65c2cb87dd564deb5612811e354eddd0b
imoongom <moongom.ca@gmail.com>
2016-01-15 16:09:36 -0800

    update comment and change computer pics

--- index.php
+++ index.php
@@ -2,50 +2,64 @@
     <head>
         <title> COMP4711 LAB1 A00907822 EUNWON MOON </title>
     </head>
-    <body>
+    <body style="text-align:center;">
      <?php
-        //initialize value when there is no board value
+        //initialize value when there is no 'board' value
         if(isset($_GET['board']))
              $squares= $_GET['board'];
         else
              $squares='---------';
-             $game = new Game($squares);
+        
+        //start game!
+        $game = new Game($squares);
 
-        class Game{
-            
+        class Game{            
             var $position;
-            //constructor
+            
+            //constructor  start Game class
             function __construct($squares){
-                echo '<h1>Welcome to the Tic-Tac-Toe Game.</h1>';
+                
+                echo '<header><h1>Welcome to the Tic-Tac-Toe Game.</h1></header>';
+                echo '<nav style="height:80px">';
+                
+                //read the string value of game playing and sprit to char array
                 $this->position = str_split($squares);
                 
-                //if the game is not just started or didnt finished, play 
+                //if the game is not just started or didnt finished, the computer play.
                 if($squares <> '---------' && !$this->endGame()){
                     $loc = $this->findPlace();
-                    $this->position[$loc]='x';
+                    $this->position[$loc]='x'; //add computer's playing
                 }
                 
-                //check the result!
-                //if the game is finished without winner
+                //check the result of the game
+                //  if the game is finished without winner
                 if($this->endGame ()){
-                    echo '<h2>GAME OVER!! DRAW!!!</h2>';
-                    echo '<button type="button"><a href="?board=---------">New Game!</a></button></br>';
-                } else if ($this->winner($this->position,'o')){     // x win
-                    echo '<h2>I win.:)</h2><button type="button"><a href="?board=---------">New Game!</a></button></br>';
-                    $this->position[$loc]='-';
+                    echo '<h2 style="color:grey;">GAME OVER!! DRAW!!!</h2>';
+                    echo '<button type="button"><a href="?board=---------">New Game!</a></button></br></nav>';
                 }
-                else if ($this->winner($this->position,'x')){   //o win
-                    echo '<h2>You win. Lucky guesses!</h2> <button type="button"><a href="?board=---------">New Game!</a></button></br>';
-                } else {        //game is not finished yet
-                    echo '<h2>No winner yet, but you are losing.</h2></br>';                                    
+                //check if the player win the game
+                else if ($this->winner($this->position,'o')){  
+                    echo '<h2 style="color:red;">You win. Lucky guesses!</h2>';
+                    echo '<button type="button"><a href="?board=---------">New Game!</a></button></br></nav>';
+                    $this->position[$loc]='-';      //if the player win, calcel the computer movement
                 }
+                //check if the computer win the game
+                else if ($this->winner($this->position,'x')){   //o win 
+                    echo '<h2 style="color:blue;">Computer win.:)</h2>';
+                    echo '<button type="button"><a href="?board=---------">New Game!</a></button></br></nav>';
+                }
+                //if the gmae is not finished keep playing
+                else {
+                    echo '<h2>No winner yet, but you are losing.</h2></br></nav>';                           
+                }
+                //show the cells
                 $this->display();
             }
             
-            //the function to check if the token is win or not
-            function winner($position,$token) {
+            //the function to check if the '$token' team is win or not
+            function winner($position, $token) {
                 $won = false;
-                //vertically
+                //vertically win check
                 for($row=0;$row<3;$row++) {
                     if (($position[3*$row] == $token) &&
                         ($position[3*$row+1] == $token) &&
@@ -53,7 +67,7 @@
                         $won = true;
                     }
                 }
-                //horizontally
+                //horizontally win check
                 for($col = 0; $col < 3; $col++){
                     if (($position[$col] == $token) &&
                     ($position[$col+3] == $token) &&
@@ -61,7 +75,7 @@
                         $won = true;
                     }
                 }
-                //diagonally
+                //diagonally  win check
                 if (($position[0] == $token) &&
                     ($position[4] == $token) &&
                     ($position[8] == $token)) {
@@ -76,7 +90,7 @@
             
             //the function to display the board
             function display() {
-                echo '<table cols="3" border="1" style="fontsize:large; fontweight:bold; text-align:center">';
+                echo '<table cols="3" border="1" style="margin:auto; fontsize:large; fontweight:bold; text-align:center">';
                 echo '<tr style="height:160">'; // open the first row
                 for ($pos=0; $pos<9;$pos++) {
                     echo $this->show_cell($pos);
@@ -87,7 +101,9 @@
                 echo '</table>';                
             }
             
-            //the function to show each cell depending on the value
+            //the function to display each cell depending on the value
+            // computer's cell is showing moose
+            // player's cell is showing olaf
             function show_cell($which) {
                 $token = $this->position[$which];
                 // deal with the easy case
@@ -95,31 +111,34 @@
                     if($token == 'o')
                         return '<td style="width:160"><img src="olaf.png"></td>';
                     else
-                        return '<td style="width:160"><img src="sven.png"></td>';
+                        return '<td style="width:160"><img src="com.png"></td>';
                 }
                 // now the hard case
                 $this->newposition = $this->position; // copy the original
                 $this->newposition[$which] = 'o'; // this would be their move
                 $move = implode($this->newposition); // make a string from the board array
                 $link = '?board='.$move; // this is what we want the link to be
-                // so return a cell containing an anchor and showing a hyphen
-                //echo 'retruning properly';
+
                 return '<td style="width:160"><a href="'.$link.'"><img src="blank.png"></a></td>';
             }
             
             //computer side algorithm to play
+            //guess the next movement and check if the spot is winnable spot
+            // or if should be blocked not to lose
+            // if neither spot is existed, choose random spot.
             function findPlace(){
-                //to find winning spot
+                
+                //to check the winning spot
                 for($i=0; $i<9; $i++){
                     if($this->position[$i] == '-'){
                         $this->newposition = $this->position;                    
                         $this->newposition[$i] = 'x';
                         if($this->winner($this->newposition,'x'))
                             return $i;
-                        
                     }
                 }
-                //to find the place to block person's win
+                
+                //to theck the competitors's winning spot
                 for($i=0; $i<9; $i++){
                     if($this->position[$i] == '-') {
                         $this->newposition = $this->position;
@@ -128,6 +147,7 @@
                             return $i;   
                     }
                 }
+                
                 //to find any blank spot
                 while($num = rand(1,9)){
                     if($this->position[--$num]=='-')
@@ -135,7 +155,8 @@
                     
                 }
             }
-            //check if the game is finished or not
+            
+            //check if the game is finished or not without winner
             function endGame(){
                 for($i=0;$i<9;$i++)
                     if($this->position[$i] == '-')
@@ -143,7 +164,6 @@
                 return true;
             }
         }
-
     ?>   
     </body>
 </html>
